2c9ba27a53a15ed1b43442d3e12a21bd49b0ad63
Yury Kuzma <vterkin1388@gmail.com>
2024-08-21 22:40:49 +0300

    create Utils.class

---
+++ core/src/main/java/Utils.java
@@ -0,0 +1,13 @@
+/*import ru.clevertec.StringUtils;
+
+public class Utils {
+    public boolean isAllPositiveNumbers(String...str) {
+        StringUtils stringUtils = new StringUtils();
+        for (String s: str) {
+            if (!stringUtils.isPositiveNumber(s)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}*/
