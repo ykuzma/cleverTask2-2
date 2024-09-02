0c45125deeba854d103cd11086ee44561288f29c
Yury Kuzma <vterkin1388@gmail.com>
2024-09-02 13:39:44 +0300

    refactor

--- api/src/test/java/ru/clevertec/api/AppTest.java
+++ api/src/test/java/ru/clevertec/api/AppTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AppTest {
+class AppTest {
 
     @Test
     void mainTest() {
         assertTrue(true);
     }
 }
+
--- core/src/main/java/ru/clevertec/core/Utils.java
+++ core/src/main/java/ru/clevertec/core/Utils.java
@@ -4,6 +4,9 @@ import ru.clevertec.StringUtils;
 import java.util.Arrays;
 
 public class Utils {
+    private Utils(){
+
+    }
 
     public static boolean isAllPositiveNumbers(String...str) throws NumberFormatException, InputDataException {
         if(str.length == 0) throw new InputDataException("Input data cannot be empty. Array length - " + str.length);
