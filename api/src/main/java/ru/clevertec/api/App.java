80df72e447634a23bad51c3849f60c976f4bd21e
Yury Kuzma <vterkin1388@gmail.com>
2024-09-10 20:51:18 +0300

    wrote tests for App.class

--- api/src/main/java/ru/clevertec/api/App.java
+++ api/src/main/java/ru/clevertec/api/App.java
@@ -5,13 +5,13 @@ import ru.clevertec.core.Utils;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(checkIsNumberPositive("12", "79"));
+        System.out.println(checkIsNumberPositive(new Utils(),"12", "79"));
     }
 
-    public static String checkIsNumberPositive(String... str) {
+    public static String checkIsNumberPositive(Utils util, String... str) {
 
         try {
-            if(Utils.isAllPositiveNumbers(str)) {
+            if(util.isAllPositiveNumbers(str)) {
                return "All numbers are positive.";
             }else {
                 return "Some numbers are not positive.";
--- api/src/test/java/ru/clevertec/api/AppTest.java
+++ api/src/test/java/ru/clevertec/api/AppTest.java
@@ -1,28 +1,42 @@
 package ru.clevertec.api;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mockito;
+import ru.clevertec.core.Utils;
 
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
-public class AppTest {
+class AppTest {
 
     @ParameterizedTest
     @MethodSource
-    void shouldReturnCorrectString(String[] numbers, String expectedString) {
+    void shouldReturnCorrectString(String[] numbers, String expectedString, boolean mockReturn) {
+        Utils util = Mockito.mock(Utils.class);
+        when(util.isAllPositiveNumbers(numbers)).thenReturn(mockReturn);
 
-        String actual = App.checkIsNumberPositive(numbers);
+        String actual = App.checkIsNumberPositive(util, numbers);
         assertEquals(expectedString, actual);
+    }
+
+    @Test
+    void whenThrowException_shouldReturnCorrectString() {
+        Utils util = Mockito.mock(Utils.class);
+        when(util.isAllPositiveNumbers()).thenThrow(new IllegalArgumentException("Empty Array"));
+
+        String actual = App.checkIsNumberPositive(util);
+        assertEquals("Something went wrong! Empty Array", actual);
     }
 
     private static Stream<Arguments> shouldReturnCorrectString(){
         return Stream.of(
-                Arguments.of(new String[]{"12", "79"}, "All numbers are positive."),
-                Arguments.of(new String[]{"-12", "79"}, "Some numbers are not positive.")
+                Arguments.of(new String[]{"12", "79"}, "All numbers are positive.", true),
+                Arguments.of(new String[]{"-12", "79"}, "Some numbers are not positive.", false)
 
         );
     }
